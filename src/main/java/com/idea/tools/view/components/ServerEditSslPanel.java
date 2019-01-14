10c0fc7eef369f21be76ebca584b3484589d8c65
kipill <kipill_90@mail.ru>
2019-01-14 23:07:13 +0100

    file dialog windows for ssl configuration

--- src/main/java/com/idea/tools/view/components/ServerEditSslPanel.java
+++ src/main/java/com/idea/tools/view/components/ServerEditSslPanel.java
@@ -2,22 +2,36 @@ package com.idea.tools.view.components;
 
 import com.idea.tools.dto.SSLConfiguration;
 import com.idea.tools.dto.ServerDto;
+import com.intellij.openapi.ui.TextFieldWithBrowseButton;
+import com.intellij.ui.MacroAwareTextBrowseFolderListener;
 
 import javax.swing.*;
+
+import static com.intellij.openapi.fileChooser.FileChooserDescriptorFactory.createSingleFileDescriptor;
 
 public class ServerEditSslPanel extends JPanel {
 
 	private JPanel rootPanel;
 	private JTextField truststorePasswordField;
 	private JTextField keystorePasswordField;
-	private JTextField truststoreField;
-	private JTextField keystoreField;
+	private TextFieldWithBrowseButton truststoreField;
+	private JPanel keystorePanel;
+	private JPanel trustStorePanel;
+	private TextFieldWithBrowseButton keystoreField;
 
 	public ServerEditSslPanel(ServerDto server) {
 		render(server);
 	}
 
 	private void render(ServerDto server) {
+		keystoreField = new TextFieldWithBrowseButton();
+		keystoreField.addBrowseFolderListener(new MacroAwareTextBrowseFolderListener(createSingleFileDescriptor(), null));
+		keystorePanel.add(keystoreField);
+
+		truststoreField = new TextFieldWithBrowseButton();
+		truststoreField.addBrowseFolderListener(new MacroAwareTextBrowseFolderListener(createSingleFileDescriptor(), null));
+		trustStorePanel.add(truststoreField);
+
 		setValues(server);
 		add(rootPanel);
 	}
@@ -37,6 +51,5 @@ public class ServerEditSslPanel extends JPanel {
 		keystoreField.setText(ssl.getKeystore());
 		keystorePasswordField.setText(ssl.getKeystorePassword());
 	}
-
 
 }
