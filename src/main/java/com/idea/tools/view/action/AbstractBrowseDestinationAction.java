c1971e5b5eed84662a1cd605ec4f236a6d51a312
kipill <kipill_90@mail.ru>
2019-07-28 19:03:22 +0200

    #7 fix for overlapping toolbar and drop-down queue browse action

--- src/main/java/com/idea/tools/view/action/AbstractBrowseDestinationAction.java
+++ src/main/java/com/idea/tools/view/action/AbstractBrowseDestinationAction.java
@@ -25,44 +25,49 @@ import static com.intellij.openapi.wm.ToolWindowAnchor.BOTTOM;
 
 public abstract class AbstractBrowseDestinationAction extends AbstractBrowserPanelAction {
 
-    private static final Icon ICON = getBrowseIcon();
-    private final Project project;
-    private Optional<QueueBrowseToolPanel> panel = Optional.empty();
+	private static final Icon ICON = getBrowseIcon();
+	private final Project project;
+	private Optional<QueueBrowseToolPanel> panel = Optional.empty();
 
-    AbstractBrowseDestinationAction(Project project, ServersBrowseToolPanel serversBrowseToolPanel) {
-        super("Browse queue", "", ICON, serversBrowseToolPanel);
-        this.project = project;
-    }
+	AbstractBrowseDestinationAction(Project project, ServersBrowseToolPanel serversBrowseToolPanel) {
+		super("Browse queue", "", ICON, serversBrowseToolPanel);
+		this.project = project;
+	}
 
-    @Override
-    public void actionPerformed(AnActionEvent e) {
-        serversPanel.getSelectedValue(DestinationDto.class)
-                .filter(d -> DestinationType.QUEUE.equals(d.getType()))
-                .ifPresent(queue -> {
-                    QueueBrowseToolPanel panel = getOrCreate();
-                    QueueBrowserTable table = panel.addQueueToBrowse(queue);
-                    new LoadMessagesTask(project, table).queue();
-                });
-    }
+	@Override
+	public void actionPerformed(AnActionEvent e) {
+		serversPanel.getSelectedValue(DestinationDto.class)
+				.filter(d -> DestinationType.QUEUE.equals(d.getType()))
+				.ifPresent(queue -> {
+					QueueBrowseToolPanel panel = getOrCreate();
+					QueueBrowserTable table = panel.addQueueToBrowse(queue);
+					new LoadMessagesTask(project, table).queue();
+				});
+	}
 
-    private QueueBrowseToolPanel getOrCreate() {
-        return panel.orElseGet(() -> {
-            QueueBrowseToolPanel panel = QueueBrowseToolPanel.of(project);
-            Content content = contentFactory().createContent(panel, null, false);
-            ToolWindowManager toolWindowManager = toolWindowManager(project);
-            ToolWindow toolWindow = toolWindowManager.registerToolWindow(JMS_MESSENGER_BROWSER_WINDOW_ID, false, BOTTOM);
-            toolWindow.setIcon(JMS_MESSENGER_BROWSER_ICON);
-            ContentManager contentManager = toolWindow.getContentManager();
-            contentManager.addContent(content);
-            this.panel = Optional.of(panel);
-            return panel;
-        });
-    }
+	private QueueBrowseToolPanel getOrCreate() {
+		return panel.orElseGet(() -> {
+			QueueBrowseToolPanel panel = QueueBrowseToolPanel.of(project);
+			Content content = contentFactory().createContent(panel, null, false);
+			ToolWindowManager toolWindowManager = toolWindowManager(project);
+			ToolWindow toolWindow = toolWindowManager.getToolWindow(JMS_MESSENGER_BROWSER_WINDOW_ID);
 
-    boolean isQueueSelected() {
-        return serversPanel.getSelectedValue(DestinationDto.class)
-                .filter(d -> DestinationType.QUEUE.equals(d.getType()))
-                .isPresent();
-    }
+			if (toolWindow == null) {
+				toolWindow = toolWindowManager.registerToolWindow(JMS_MESSENGER_BROWSER_WINDOW_ID, false, BOTTOM);
+				toolWindow.setIcon(JMS_MESSENGER_BROWSER_ICON);
+			}
+
+			ContentManager contentManager = toolWindow.getContentManager();
+			contentManager.addContent(content);
+			this.panel = Optional.of(panel);
+			return panel;
+		});
+	}
+
+	boolean isQueueSelected() {
+		return serversPanel.getSelectedValue(DestinationDto.class)
+				.filter(d -> DestinationType.QUEUE.equals(d.getType()))
+				.isPresent();
+	}
 
 }
