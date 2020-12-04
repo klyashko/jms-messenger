e35e1359ab252ba9ee11f72a0a7d321ff6ed12d2
Lyashko Kirill <klyashko@zetaglobal.com>
2020-12-04 14:52:17 +0100

    #10 IntelliJ Crash

--- src/main/java/com/idea/tools/utils/GuiUtils.java
+++ src/main/java/com/idea/tools/utils/GuiUtils.java
@@ -24,88 +24,89 @@ import javax.swing.text.JTextComponent;
 
 public class GuiUtils {
 
-	public static ToolWindowManager toolWindowManager(Project project) {
-		return ToolWindowManager.getInstance(project);
-	}
+    public static ToolWindowManager toolWindowManager(Project project) {
+        return ToolWindowManager.getInstance(project);
+    }
 
-	public static StartupManager startupManager(Project project) {
-		return StartupManager.getInstance(project);
-	}
+    public static StartupManager startupManager(Project project) {
+        return StartupManager.getInstance(project);
+    }
 
-	public static ContentFactory contentFactory() {
-		return ContentFactory.SERVICE.getInstance();
-	}
+    public static ContentFactory contentFactory() {
+        return ContentFactory.SERVICE.getInstance();
+    }
 
-	public static boolean showYesNoDialog(String msg) {
-		return YES_OPTION == showConfirmDialog(null, msg, "Confirm", YES_NO_OPTION, WARNING_MESSAGE);
-	}
+    public static boolean showYesNoDialog(String msg) {
+        return YES_OPTION == showConfirmDialog(null, msg, "Confirm", YES_NO_OPTION, WARNING_MESSAGE);
+    }
 
-	public static JFormattedTextField createNumberInputField() {
-		NumberFormat format = NumberFormat.getIntegerInstance();
-		format.setGroupingUsed(false);
-		return new JFormattedTextField(format);
-	}
+    public static JFormattedTextField createNumberInputField() {
+        NumberFormat format = NumberFormat.getIntegerInstance();
+        format.setGroupingUsed(false);
+        return new JFormattedTextField(format);
+    }
 
-	public static void installActionGroupInToolBar(ActionGroup actionGroup,
-												   SimpleToolWindowPanel toolWindowPanel,
-												   String toolBarName) {
+    public static void installActionGroupInToolBar(ActionGroup actionGroup,
+                                                   SimpleToolWindowPanel toolWindowPanel,
+                                                   String toolBarName) {
 
-		JComponent actionToolbar = ActionManager.getInstance()
-				.createActionToolbar(toolBarName, actionGroup, true)
-				.getComponent();
-		toolWindowPanel.setToolbar(actionToolbar);
-	}
+        JComponent actionToolbar = ActionManager.getInstance()
+                .createActionToolbar(toolBarName, actionGroup, true)
+                .getComponent();
 
-	public static JComponent toolbar(ActionGroup actions, String place, boolean horizontal) {
-		return ActionManager.getInstance().createActionToolbar(place, actions, horizontal).getComponent();
-	}
+        runInSwingThread(() -> toolWindowPanel.setToolbar(actionToolbar));
+    }
 
-	public static <T> JLabel label(T value, Function<T, String> function) {
-		if (value != null) {
-			return new JLabel(function.apply(value));
-		}
-		return new JLabel();
-	}
+    public static JComponent toolbar(ActionGroup actions, String place, boolean horizontal) {
+        return ActionManager.getInstance().createActionToolbar(place, actions, horizontal).getComponent();
+    }
 
-	public static void showDialog(JFrame dialog, String title) {
-		SwingUtilities.invokeLater(() -> {
-			dialog.setLocationRelativeTo(null);
-			dialog.setTitle(title);
-			dialog.pack();
-			dialog.setVisible(true);
-		});
-	}
+    public static <T> JLabel label(T value, Function<T, String> function) {
+        if (value != null) {
+            return new JLabel(function.apply(value));
+        }
+        return new JLabel();
+    }
 
-	public static void runInSwingThread(Runnable runnable) {
-		Application application = getApplication();
-		if (application.isDispatchThread()) {
-			runnable.run();
-		} else {
-			application.invokeAndWait(runnable);
-		}
-	}
+    public static void showDialog(JFrame dialog, String title) {
+        SwingUtilities.invokeLater(() -> {
+            dialog.setLocationRelativeTo(null);
+            dialog.setTitle(title);
+            dialog.pack();
+            dialog.setVisible(true);
+        });
+    }
 
-	public static void addSimpleListener(JTextComponent component, Consumer<DocumentEvent> consumer) {
-		component.getDocument().addDocumentListener(simpleListener(consumer));
-	}
+    public static void runInSwingThread(Runnable runnable) {
+        Application application = getApplication();
+        if (application.isDispatchThread()) {
+            runnable.run();
+        } else {
+            application.invokeAndWait(runnable);
+        }
+    }
 
-	public static DocumentListener simpleListener(Consumer<DocumentEvent> consumer) {
-		return new DocumentListener() {
-			@Override
-			public void insertUpdate(DocumentEvent e) {
-				consumer.accept(e);
-			}
+    public static void addSimpleListener(JTextComponent component, Consumer<DocumentEvent> consumer) {
+        component.getDocument().addDocumentListener(simpleListener(consumer));
+    }
 
-			@Override
-			public void removeUpdate(DocumentEvent e) {
-				consumer.accept(e);
-			}
+    public static DocumentListener simpleListener(Consumer<DocumentEvent> consumer) {
+        return new DocumentListener() {
+            @Override
+            public void insertUpdate(DocumentEvent e) {
+                consumer.accept(e);
+            }
 
-			@Override
-			public void changedUpdate(DocumentEvent e) {
-				consumer.accept(e);
-			}
-		};
-	}
+            @Override
+            public void removeUpdate(DocumentEvent e) {
+                consumer.accept(e);
+            }
+
+            @Override
+            public void changedUpdate(DocumentEvent e) {
+                consumer.accept(e);
+            }
+        };
+    }
 
 }
